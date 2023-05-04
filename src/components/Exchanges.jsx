494dfc8078a223ce9bd928285c8521a0cab811d6
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-05-04 22:54:22 +0530

    add header

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { server } from '..'
+import { useStatStyles } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
+import { LoaderFunction } from 'react-router-dom'
 
 const Exchanges = () => {
+    const [exchanges, setExchanges]=useState([]);
+    const [loading, setLoading] =useState(true)
+    useEffect(() => {
+      const fetchExchanges=async()=>{
+        const {data} = await axios.get(`${server}/exchanges`)
+        setExchanges(data); 
+        setLoading(false);
+      };
+      fetchExchanges();
+    
+    }, [])
+    
   return (
-    <div>Exchanges</div>
+    <Container maxW={"container.xl"}>
+        
+
+    </Container>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
+import { HStack } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '@chakra-ui/react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
+        <Button variant={"unstyled"} color={"white"}>
+            <Link to={'/'}>Home</Link>
+        </Button>
+        <Button variant={"unstyled"} color={"white"}>
+            <Link to={'/exchanges'}>Exchanges</Link>
+        </Button>
+        <Button variant={"unstyled"} color={"white"}>
+            <Link to={'/coins'}>Coins</Link>
+        </Button>
+
+    </HStack>
   )
 }
 
